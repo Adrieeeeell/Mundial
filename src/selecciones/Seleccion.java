805953e0a6f9d093d55d393490e37dd864aefc27
MarianaM <mejiagonzalezmariana007@gmail.com>
2026-06-18 16:16:55 -0600

    Clase Seleccion
    Se le añadieron todos los diferentes metodos

--- src/selecciones/Seleccion.java
+++ src/selecciones/Seleccion.java
@@ -4,10 +4,53 @@
  */
 package selecciones;
 
+import Entrenadores.Entrenador;
+import Jugadores.Jugador;
+
+
 /**
  *
  * @author HP
  */
 public class Seleccion {
-    
+
+    public enum Grupo {
+        A, B, C, D, E, F, G, H
+    }
+
+    private String nombre;
+    private Grupo grupo;
+    private Entrenador entrenador;
+    private Jugador[] jugadores;
+    private int contador;
+
+    public Seleccion(String nombre, Grupo grupo, Entrenador entrenador, int capacidad) {
+        this.nombre = nombre;
+        this.grupo = grupo;
+        this.entrenador = entrenador;
+        this.jugadores = new Jugador[capacidad];
+        this.contador = 0;
+    }
+
+    public void agregarJugador(Jugador jugador) {
+        if (contador < jugadores.length) {
+            jugadores[contador] = jugador;
+            contador++;
+        }
+    }
+
+    public void eliminarJugador(Jugador jugador) {
+        for (int i = 0; i < contador; i++) {
+            if (jugadores[i].equals(jugador)) {
+                jugadores[i] = jugadores[contador - 1];
+                jugadores[contador - 1] = null;
+                contador--;
+                break;
+            }
+        }
+    }
+
+    public Jugador[] getJugadores() {
+        return jugadores;
+    }
 }
