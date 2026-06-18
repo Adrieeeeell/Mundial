946047e30a061c8e132593de9348235cbea28b52
Adriel <adrielvillegaslopez@gmail.com>
2026-06-18 14:31:19 -0600

    Se crea Entrenador
    Se crea y programa la clase entrenador la cual extiende las coicidencias general de otras clases como persona

---
+++ src/Entrenadores/Entrenador.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entrenadores;
+
+import Personas.Persona;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Student
+ */
+public abstract class Entrenador extends Persona{
+    
+    private String Experiencia;
+
+public Entrenador(String Experiencia, String nombre, String nacionalidad, String Fecha) {
+        super(nombre, nacionalidad, Fecha);
+        this.Experiencia = Experiencia;
+    }
+  
+    public String getExperiencia() {
+        return Experiencia;
+    }
+
+    public void setExperiencia(String Experiencia) {
+        this.Experiencia = Experiencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Entrenador"+getNombre()+"Nacionalidad"+getNacionalidad()+"Edad"+getFecha()+"Experiencia"+Experiencia;
+    }
+    
+}
