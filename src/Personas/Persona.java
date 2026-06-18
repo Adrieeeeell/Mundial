ecd2ec2bd2a65ae29bdb32b098abe685e3b90289
nwvei <aaronvillegasl04@gmail.com>
2026-06-18 14:22:01 -0600

    Se hizo la clase abstracta Persona Arreglado
    Se hizo la clase abstract persona pra arbitro,jugador y entrenador, con sus metodos get y set

--- src/Personas/Persona.java
+++ src/Personas/Persona.java
@@ -8,6 +8,39 @@ package Personas;
  *
  * @author Aaron
  */
-public class Persona {
+public abstract class Persona {
+    protected String nombre;
+    protected String nacionalidad;
+    protected String Fecha;
+
+    public Persona(String nombre, String nacionalidad, String Fecha) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.Fecha = Fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public void setFecha(String Fecha) {
+        this.Fecha = Fecha;
+    }
     
 }
