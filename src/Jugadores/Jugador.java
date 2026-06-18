179f59e832f27c81fa349096d069f2aede7b1d1b
Annia Mai <anmaich@est.utn.ac.cr>
2026-06-18 14:55:50 -0600

    Clase jugador
    Se agrega los componentes de clase jugador

---
+++ src/Jugadores/Jugador.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Jugadores;
+import java.time.LocalDate;
+import Personas.Persona;
+
+/**
+ *
+ * @author UTN
+ */
+public class Jugador extends Persona {
+    private final int numero;
+    private String posicion;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public Jugador(String nombre, String nacionalidad, LocalDate fecha, int numero, String posicion) {
+        super(nombre, nacionalidad, fecha);
+        this.numero = numero;
+        this.posicion = posicion;
+    }
+
+    public Jugador(String nombre, String nacionalidad, LocalDate fecha, int numero) {
+        super(nombre, nacionalidad, fecha);
+        this.numero = numero;
+        this.posicion = "No asignada";
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador \nNombre: " + getNombre() +
+               "\nNacionalidad: " + getNacionalidad() +
+                "\nEdad: " + getFecha() + "\nNumero: " +
+                this.numero + "\nPosicion: " + this.posicion;
+    }
+    
+    
+    
+    
+}
