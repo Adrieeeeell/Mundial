2abfe3fcff1d0296362e3d976b2ec0e75f77d29c
nwvei <aaronvillegasl04@gmail.com>
2026-06-18 14:18:54 -0600

    Se hizo la clase abstracta Persona
    Se hizo la clase abstract persona pra arbitro,jugador y entrenador, con sus metodos get y set

--- src/Arbitros/Arbitro.java
+++ src/Arbitros/Arbitro.java
@@ -2,12 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package jugadores;
+package Arbitros;
+
+import java.time.LocalDate;
 
 /**
  *
- * @author UTN
+ * @author Aaron
  */
-public class Jugador {
+public class Arbitro {
+    private String nombre;
+    private String Nacionalidad;
+    private LocalDate Fecha;
     
 }
