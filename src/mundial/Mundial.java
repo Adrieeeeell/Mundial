489a309e2b244008c90e30585a88925ee8173c41
Adriel <adrielvillegaslopez@gmail.com>
2026-06-18 14:00:23 -0600

    Primer Clase Realizada mundial
    Se crea la primera clase en la cual se programa los basico la informacion del mundial

--- src/mundial/Mundial.java
+++ src/mundial/Mundial.java
@@ -9,5 +9,31 @@ package mundial;
  * @author Student
  */
 public class Mundial {
+    private String Nombre;
+    private String Ano;
+    private String Pais;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getAno() {
+        return Ano;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public Mundial(String Nombre, String Ano, String Pais) {
+        this.Nombre = "Fifa World Cup";
+        this.Ano = "2026";
+        this.Pais = "Estados unidos/Canada/Mexico";
+    }
+
+    @Override
+    public String toString() {
+        return "Mundial{" + "Nombre=" + Nombre + ", Año=" + Ano + ", Pais=" + Pais + '}';
+    }
     
 }
